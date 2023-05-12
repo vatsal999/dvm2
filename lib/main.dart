aa29d500f63954e5fba83231029deec6c0aa28d8
vatsal999 <68118780+vatsal999@users.noreply.github.com>
2023-05-12 22:17:20 +0530

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:dvm2/screens/home.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
